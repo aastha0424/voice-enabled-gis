1735f8a4ce9eb629e3d986725832b386b56a7e66
Aastha Singh <143836559+aastha0424@users.noreply.github.com>
2024-08-08 18:02:49 +0530

    wms commit

--- src/js/wms_layers.js
+++ src/js/wms_layers.js
@@ -27,6 +27,34 @@ export const wmsLayers = [
             'FORMAT': 'image/png'
         },
         attributions: '© Bhuvan'
+    },
+    {
+        title: 'Assam LULC',
+        url: 'https://bhuvan-vec2.nrsc.gov.in/bhuvan/gwc/service/wmts/',
+        layer: 'lulc:AS_LULC50K_1516',
+        matrixSet: 'EPSG:4326',
+        format: 'image/png',
+        projection: 'EPSG:4326',
+        tileGrid: new ol.tilegrid.WMTS({
+            origin: [-180.0, 90.0],
+            resolutions: [
+                0.703125, 0.3515625, 0.17578125, 0.087890625, 0.0439453125,
+                0.02197265625, 0.010986328125, 0.0054931640625, 0.00274658203125, 
+                0.001373291015625, 0.0006866455078125, 0.00034332275390625, 
+                0.000171661376953125, 0.0000858306884765625, 0.00004291534423828125
+            ],
+            matrixIds: [
+                'EPSG:4326:0', 'EPSG:4326:1', 'EPSG:4326:2', 'EPSG:4326:3',
+                'EPSG:4326:4', 'EPSG:4326:5', 'EPSG:4326:6', 'EPSG:4326:7',
+                'EPSG:4326:8', 'EPSG:4326:9', 'EPSG:4326:10', 'EPSG:4326:11',
+                'EPSG:4326:12', 'EPSG:4326:13', 'EPSG:4326:14'
+            ]
+        }),
+        params: {
+            'LAYERS': 'lulc:AS_LULC50K_1516',
+            'FORMAT': 'image/png'
+        },
+        attributions: '© Bhuvan'
     }
     // Add more WMS layers here as needed
 ];
